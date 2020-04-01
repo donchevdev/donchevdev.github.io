323c911d089e40fe436295554de52c495649954d
donchevdev <donchevdev@gmail.com>
2020-04-01 12:10:27 +0300

    Add tablet navigation

--- js/functions.js
+++ js/functions.js
@@ -76,6 +76,20 @@ $('.nav-dropdown').each(function() {
 });
 
 /*
+ * Touch dropdown
+ */
+$('.has-dd > a').on('touchstart', function(e) {
+	var $parent 		  = $(this).parent();
+	var navDDVisibleClass = 'nav-dropdown-visible';
+
+	if (!$parent.hasClass(navDDVisibleClass)) {
+		e.preventDefault();
+
+		$parent.addClass(navDDVisibleClass);
+	}
+});
+
+/*
  * Init scroll animator
  */
 new ScrollAnimator($('.animate'), {
